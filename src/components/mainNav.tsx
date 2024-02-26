87bd43735421af1d34c17b2ce545df08e585c5e8
argonarch <armando12123434@gmail.com>
2024-02-26 18:12:29 -0300

    translate texts

--- src/components/mainNav.tsx
+++ src/components/mainNav.tsx
@@ -6,14 +6,14 @@ export default function mainNav() {
   return (
     <nav className="backdrop-blur-md bg-black/40 w-screen absolute text-white">
       <div className="md:container">
-        <div className="flex space-x-2 p-1">
-          <div className="flex">
-            <a href="#" className="text-xl">
+        <div className="flex space-x-2 p-1.5">
+          <div className="flex items-center">
+            <a href="/" className="text-xl font-bold">
               Home
             </a>
           </div>
           <div className="flex grow justify-center"></div>
-          <div className="flex gap-2">
+          <div className="flex gap-x-3">
             <a href={siteConfig.links.github}>
               <Icons.gitHub className={sizeIcon} />
             </a>
