2eb03aa712e9f9f775632dc24d03fa90346d9e72
argonarch <armando12123434@gmail.com>
2024-03-06 14:58:41 -0300

    update link curriculum

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -2,7 +2,7 @@ export const siteConfig = {
     links: {
       github: "https://github.com/argonarch",
       whatsapp: "https://api.whatsapp.com/send?phone=541122559077",
-      curriculum: "https://drive.google.com/file/d/1B-mlFK9Gg9rDI9PvpndBde2C54y0yhxc/view?usp=sharing",
+      curriculum: "https://drive.google.com/file/d/1Xl36xIJsmLrxs-OIMVcyXJuWZJcKZfHV/view?usp=sharing",
     },
   }
   
