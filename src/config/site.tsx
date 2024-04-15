ecb31e03620eb8f45e97c0916dfbd487b45992c9
Armando Escobar <51279889+argonarch@users.noreply.github.com>
2024-04-14 21:42:25 -0300

    Update site.tsx
    change drive curriculum link

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -2,7 +2,7 @@ export const siteConfig = {
     links: {
       github: "https://github.com/argonarch",
       whatsapp: "https://api.whatsapp.com/send?phone=541122559077",
-      curriculum: "https://drive.google.com/file/d/1Xl36xIJsmLrxs-OIMVcyXJuWZJcKZfHV/view?usp=sharing",
+      curriculum: "https://drive.google.com/file/d/1GARMztU-6Y3Zs4ukVXOVyPBPrV6D-C9h/view?usp=sharing",
     },
   }
   
