301fd9722cbf4102035cc68276feb9d4feafcf0b
Armantex <106185913+Armantex@users.noreply.github.com>
2024-05-17 19:39:51 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://argonarch.me',
+  site: 'https://Armantex.github.io',
+  base: '',
   integrations: [tailwind(), react()],
 });
